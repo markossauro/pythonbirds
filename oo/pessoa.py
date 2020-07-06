fc5a33309bc90f6e20aa37af36fe10ef5700542b
markossauro <j.marcos.jmrf@gmail.com>
2020-07-06 16:22:45 -0300

    criada atributos instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,1 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'olá {self.nome} sua idade é {self.idade} anos'
+
+
+if __name__ == '__main__':
+
+    nome = input("Insira seu nome: ")
+    idade = input("insira sua idade: ")
+
+    p = Pessoa(nome,idade)
+    print(p.cumprimentar())
+
